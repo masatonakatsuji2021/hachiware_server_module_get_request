a2733516169856a1002cf1ac461d3c721e11431a
nakajima-satoru <nakajima@email.com>
2022-01-18 21:24:49 +0900

    1/18

--- index.js
+++ index.js
@@ -4,7 +4,11 @@
  * 
  * Module for receiving request data (GET / POST request) of Web server package "hachiware_server" .
  * 
- * Author : Nakatsuji Masato 
+ * License : MIT License. 
+ * Since   : 2022.01.15
+ * Author  : Nakatsuji Masato 
+ * GitHub  : https://github.com/masatonakatsuji2021/Hachiware_Server_module_get_request
+ * npm     : https://www.npmjs.com/package/Hachiware_Server_module_get_request
  * ============================================================================================================
  */
 
