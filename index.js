69050f55c252210251e5c254438448aa3a46ea29
nakajima-satoru <nakajima@email.com>
2022-02-26 13:01:47 +0900

    2/26

--- index.js
+++ index.js
@@ -106,22 +106,6 @@ module.exports = function(conf){
             return resolve();
         }
 
-        /*
-        var url = req.url;
-        var queryStr = url.split("?")[1];
-        var query = {};
-
-        if(queryStr){
-            var qbuff = queryStr.split("&");
-            for(var n = 0 ; n < qbuff.length ; n++){
-                var q_ = qbuff[n].split("=");
-
-                query[q_[0]] = q_[1];
-            }
-        }
-        req.query = query;
-        */
-
         var query = getQuery(req);
         req.query = query;
 
@@ -136,43 +120,6 @@ module.exports = function(conf){
                 req.body = body;
                 resolve();
             });
-
-            /*
-            var bodyBuff = null;
-            req.on("data",function(data){
-                if(!bodyBuff){
-                    bodyBuff = "";
-                }
-                bodyBuff += data;
-            }).on("end", function(){
-
-                var contentType = req.headers["content-type"];
-                var body = null;
-
-                if(contentType == "application/json"){
-                    body = JSON.parse(bodyBuff);
-                }
-                else if(contentType == "application/x-www-form-urlencoded"){
-                    body = {};
-                    var bbuff = bodyBuff.split("&");
-                    for(var n = 0 ; n < bbuff.length ; n++){
-                        var b_ = bbuff[n].split("=");
-                        body[b_[0]] = b_[1];
-                    }
-                }
-                else if(contentType == "multipart/form-data"){
-                    //......
-                    body = bodyBuff;
-                }
-                else{
-                    body = bodyBuff;
-                }
-
-                req.body = body;
-
-                resolve();
-            });
-            */
         }
 
     };
